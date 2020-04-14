7a42c6f285a357fa936745e7f9084c1a288cc5a5
bluephoenix2507 <theo2507@hotmail.fr>
2020-04-14 15:51:14 +0200

    Début

--- app/src/main/java/com/agendize/android/blackjack/game/Deck.java
+++ app/src/main/java/com/agendize/android/blackjack/game/Deck.java
@@ -1,4 +1,25 @@
 package com.agendize.android.blackjack.game;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
+    private List<Card> cards;
+    private Player currentCard;
+
+    public Deck() {
+        cards = new ArrayList<>(52);
+    }
+    public void shuffleCards() {
+        Collections.shuffle(cards, new Random());
+        int currentCard = 0;
+    }
+    public Card getNextCard() {
+        Card card = cards.get(currentCard);
+        int currentCard;
+        currentCard++;
+        return card;
+    }
 }
--- app/src/main/java/com/agendize/android/blackjack/game/Player.java
+++ app/src/main/java/com/agendize/android/blackjack/game/Player.java
@@ -1,4 +1,9 @@
 package com.agendize.android.blackjack.game;
 
 public class Player {
+    private void currentCard(){
+
+
+    }
 }
+
